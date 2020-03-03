0cdb791d87714fcd12f51224b84b7103a6d37ffd
Roy <roymeplease@gmail.com>
2020-03-03 13:50:46 +0200

    added a checked signal to each element when pressing the DONE button

---
+++ my-todo-list.js
@@ -0,0 +1,29 @@
+const done = document.querySelectorAll(".todo-change-action");
+const remove = document.querySelectorAll(".todo-remove")
+
+
+
+done.forEach(element => {
+    element.addEventListener("click", checked);
+})
+remove.forEach(element => {
+    element.addEventListener("click", removeItem);
+})
+
+
+function checked(event) {
+    event.preventDefault();
+    let checked = this.previousElementSibling;
+    //with the while loop, we're trying to get to the 'check' icon.
+    while(checked.getAttribute("class") === null ||!checked.getAttribute("class").includes("check")) {
+        checked = checked.previousElementSibling;
+    }
+    checked.style.display = "block";
+}
+
+function removeItem(event) {
+    event.preventDefault();
+    const classAtt = this.getAttribute("class");
+    console.log(classAtt);
+    document.querySelector(".todo-item").remove(classAtt);
+}
