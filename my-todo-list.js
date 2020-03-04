9d93c27bc4d1348c2d9d6e38aefda2cbfb2e743b
Roy <roymeplease@gmail.com>
2020-03-05 00:20:36 +0200

    added localstorage so when the user presses DONE the check marks stays

--- my-todo-list.js
+++ my-todo-list.js
@@ -1,7 +1,11 @@
 const done = document.querySelectorAll(".todo-change-action");
-const remove = document.querySelectorAll(".todo-remove")
+const remove = document.querySelectorAll(".todo-remove");
+const vi = document.querySelectorAll(".check");
+
+console.log(vi);
 
 
+// localStorage.clear();
 
 done.forEach(element => {
     element.addEventListener("click", checked);
@@ -15,11 +19,41 @@ function checked(event) {
     event.preventDefault();
     let checked = this.previousElementSibling;
     //with the while loop, we're trying to get to the 'check' icon.
-    while(checked.getAttribute("class") === null ||!checked.getAttribute("class").includes("check")) {
+    while (checked.getAttribute("class") === null || !checked.getAttribute("class").includes("check")) {
         checked = checked.previousElementSibling;
     }
     checked.style.display = "block";
+    setStorage(vi);
 }
+
+
+function setStorage(nodeList) {
+    const newArr = []
+    nodeList.forEach((item, index) => {
+        if (window.getComputedStyle(item).display === "block") {
+            newArr.push(index);
+        }
+    })
+    let stringifiedArr = JSON.stringify(newArr);
+    localStorage.setItem("viList", stringifiedArr);
+}
+
+function readStorage(nodeList) {
+    let getViList = localStorage.getItem("viList");
+    let unstringfiedArr = JSON.parse(getViList);
+    if (unstringfiedArr === null) {
+        return;
+    }
+    nodeList.forEach((item, index) => {
+        if (unstringfiedArr.includes(index)) {
+            item.style.display = "block";
+        }
+    });
+}
+
+readStorage(vi);
+
+
 
 function removeItem(event) {
     event.preventDefault();
